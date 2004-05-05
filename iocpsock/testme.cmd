0ca9d1eb60b0856b3cb19ed92b578b7854359bcc
davygrvy <>
2004-05-05 01:29:58 +0000

    more tunable for the user

--- iocpsock/testme.cmd
+++ iocpsock/testme.cmd
@@ -1,17 +1,20 @@
 @echo off
-::Launch the test suite!
+:: Launch the test suite!
 
 if not defined MSDevDir (
-    call c:\dev\devstudio60\vc98\bin\vcvars32.bat
+    ::call "C:\Program Files\Microsoft Developer Studio\vc\bin\vcvars32.bat"
+    ::call c:\dev\devstudio60\vc98\bin\vcvars32.bat
+    call "C:\Program Files\Microsoft Developer Studio\vc98\bin\vcvars32.bat"
 )
 
 if not defined MSSdk (
-    call c:\dev\platfo~1\SetEnv.bat /RETAIL
+    ::call c:\dev\platsdk\SetEnv.bat /RETAIL
+    call "C:\Program Files\Microsoft SDK\SetEnv.bat" /RETAIL
 )
 
 if not defined TCLDIR (
     set TCLDIR=d:\tcl_workspace\tcl_84_branch
 )
 
-nmake -nologo -f makefile.vc test
+nmake -nologo -csf makefile.vc test
 pause
