59c635bc6c15f213653c45b0fc2fe3d62e564cba
davygrvy <>
2005-03-24 19:48:24 +0000

    Fixed format of addr->irdaDeviceId so it is displayed similar to a MAC address (XX-XX-XX-XX)

--- iocpsock/ws2irda.c
+++ iocpsock/ws2irda.c
@@ -21,12 +21,16 @@ static WS2ProtocolData irdaProtoData = {
 static Tcl_Obj *
 DecodeIrdaSockaddr (SocketInfo *info, LPSOCKADDR addr)
 {
+    char formatedId[12];
     Tcl_Obj *result = Tcl_NewObj();
     SOCKADDR_IRDA *irdaaddr = (SOCKADDR_IRDA *) addr;
 
     /* Device ID. */
-    Tcl_ListObjAppendElement(NULL, result, Tcl_NewLongObj(
-	    *((long *)irdaaddr->irdaDeviceID)));
+    sprintf(formatedId, "%02x-%02x-%02x-%02x",
+	    irdaaddr->irdaDeviceID[0], irdaaddr->irdaDeviceID[1],
+	    irdaaddr->irdaDeviceID[2], irdaaddr->irdaDeviceID[3]);
+    Tcl_ListObjAppendElement(NULL, result,
+	    Tcl_NewStringObj(formatedId, 11));
 
     /* Service Name (probably not in UTF-8). */
     Tcl_ListObjAppendElement(NULL, result, Tcl_NewStringObj(
@@ -52,6 +56,7 @@ Iocp_IrdaDiscovery (Tcl_Interp *interp, Tcl_Obj **deviceList, int limit)
     const char *hints2[] = {
 	"Telephony", "Server", "Comm", "Message", "HTTP", "OBEX", NULL
     };
+    char formatedId[12];
     Tcl_DString deviceDString;
 
     /*
@@ -87,13 +92,17 @@ Iocp_IrdaDiscovery (Tcl_Interp *interp, Tcl_Obj **deviceList, int limit)
     /*
      * Create the output Tcl_Obj, if none exists there.
      */
+
     if (*deviceList == NULL) {
 	*deviceList = Tcl_NewObj();
     }
 
     for (i = 0; i < deviceListStruct->numDevice; i++) {
 	thisDevice = deviceListStruct->Device+i;
-	entry[0] = Tcl_NewLongObj(*((long*)thisDevice->irdaDeviceID));
+	sprintf(formatedId, "%02x-%02x-%02x-%02x",
+		thisDevice->irdaDeviceID[0], thisDevice->irdaDeviceID[1],
+		thisDevice->irdaDeviceID[2], thisDevice->irdaDeviceID[3]);
+	entry[0] = Tcl_NewStringObj(formatedId, 11);
 	charSet = (thisDevice->irdaCharSet) & 0xff;
 	switch (charSet) {
 	    case 0xff:
@@ -102,7 +111,7 @@ Iocp_IrdaDiscovery (Tcl_Interp *interp, Tcl_Obj **deviceList, int limit)
 		nameEnc = "ascii"; break;
 	    default:
 		nameEnc = isocharset; 
-		isocharset[9] = charSet+'0';
+		isocharset[9] = charSet + '0';
 		break;
 	}
 	enc = Tcl_GetEncoding(interp, nameEnc);
@@ -115,16 +124,18 @@ Iocp_IrdaDiscovery (Tcl_Interp *interp, Tcl_Obj **deviceList, int limit)
 		Tcl_DStringLength(&deviceDString));
 	Tcl_DStringFree(&deviceDString);
 	entry[2] = Tcl_NewObj();
-	for (bit=0; hints1[bit]; ++bit) 
+	for (bit=0; hints1[bit]; ++bit) {
 	    if (thisDevice->irdaDeviceHints1 & (1<<bit))
-		Tcl_ListObjAppendElement(interp,entry[2],
+		Tcl_ListObjAppendElement(interp, entry[2],
 			Tcl_NewStringObj(hints1[bit],-1));
-	for (bit=0; hints2[bit]; ++bit) 
+	}
+	for (bit=0; hints2[bit]; ++bit) {
 	    if (thisDevice->irdaDeviceHints2 & (1<<bit))
-		Tcl_ListObjAppendElement(interp,entry[2],
+		Tcl_ListObjAppendElement(interp, entry[2],
 			Tcl_NewStringObj(hints2[bit],-1));
-	Tcl_ListObjAppendElement(interp,*deviceList,
-		Tcl_NewListObj(3,entry));
+	}
+	Tcl_ListObjAppendElement(interp, *deviceList,
+		Tcl_NewListObj(3, entry));
     }
 
     ckfree((char *)deviceListStruct);
