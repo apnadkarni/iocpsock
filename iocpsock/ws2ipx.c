b079cf2806fe4d9bfc1cd4d66af1cb8ab8aaec32
davygrvy <>
2006-03-08 20:08:49 +0000

    Added DecodeIpxSockaddr() according to an example I found for how it should be displayed

--- iocpsock/ws2ipx.c
+++ iocpsock/ws2ipx.c
@@ -82,5 +82,19 @@ static WS2ProtocolData spx2StreamProtoData = {
 static Tcl_Obj *
 DecodeIpxSockaddr (SocketInfo *info, LPSOCKADDR addr)
 {
-    return Tcl_NewObj();
+    static char dest[128];
+    LPSOCKADDR_IPX ipxadddr = (LPSOCKADDR_IPX) addr;
+    wsprintf(dest, "%02X%02X%02X%02X.%02X%02X%02X%02X%02X%02X:%04X", 
+        (unsigned char)ipxadddr->sa_netnum[0],
+        (unsigned char)ipxadddr->sa_netnum[1],
+        (unsigned char)ipxadddr->sa_netnum[2],
+        (unsigned char)ipxadddr->sa_netnum[3],
+        (unsigned char)ipxadddr->sa_nodenum[0],
+        (unsigned char)ipxadddr->sa_nodenum[1],
+        (unsigned char)ipxadddr->sa_nodenum[2],
+        (unsigned char)ipxadddr->sa_nodenum[3],
+        (unsigned char)ipxadddr->sa_nodenum[4],
+        (unsigned char)ipxadddr->sa_nodenum[5],
+        winSock.ntohs(ipxadddr->sa_socket));
+    return Tcl_NewStringObj(dest, -1);
 }
