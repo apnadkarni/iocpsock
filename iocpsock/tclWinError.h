eb373961f6f3432e73e52ea2ee9903397c9b1ce8
davygrvy <>
2006-09-15 17:28:42 +0000

    not needed anymore

--- iocpsock/tclWinError.h
+++
@@ -1,4 +0,0 @@
-CONST char *Tcl_Win32ErrId (unsigned long errorCode);
-CONST char *Tcl_Win32ErrMsg TCL_VARARGS(unsigned long,errorCode);
-CONST char *Tcl_Win32ErrMsgVA (unsigned long errorCode, va_list argList);
-CONST char *Tcl_Win32Error TCL_VARARGS(Tcl_Interp *,interp);
